cfa7296dc5aa4665d760e615dace5761a9c00582
Patryk Ptasinski <patryk@ipepe.pl>
2018-12-30 16:06:44 +0100

    Added live coordinates

--- docs/application.js
+++ docs/application.js
@@ -1,4 +1,68 @@
 $(function(){
+
+    L.Control.LiveMouseCoordinatesControl = L.Control.extend({
+        options: {
+            position: 'bottomright',
+            labelTemplate: "Coordinates: {0}",
+            labelFormatter: undefined
+        },
+
+        onAdd: function(map){
+            this._map = map;
+
+            var className = 'leaflet-control-mapcodes',
+                container = this._container = L.DomUtil.create('div', className),
+                options = this.options;
+
+            // label container
+            this._labelcontainer = L.DomUtil.create("div", "uiElement label", container);
+            this._label = L.DomUtil.create("span", "labelFirst", this._labelcontainer);
+
+            // connect to mouseevents
+            map.on("mousemove", this._update, this);
+            map.whenReady(this._update, this);
+
+            // return the container
+            return container;
+        },
+
+        onRemove: function(map){
+            map.off("mousemove", this._update, this);
+        },
+
+        _update: function(evt){
+            var pos = evt.latlng;
+            var opts = this.options;
+
+            if (pos) {
+                pos = pos.wrap();
+                var mapcode = this._LatLngToMapcode(pos);
+                this._label.innerHTML = this._createMapcodeLabel(mapcode);
+            }
+        },
+
+        _LatLngToMapcode: function(point){
+            var lng = point.lng;
+            var lat = point.lat;
+            return lat.toFixed(1) + ', ' + lng.toFixed(1);
+        },
+
+        _createMapcodeLabel: function(mapcode){
+            var opts = this.options, l;
+            if (opts.labelFormatter) {
+                l = opts.labelFormatter(mapcode);
+            } else {
+                l = L.Util.template(opts.labelTemplate, {0: mapcode});
+            }
+            return l;
+        }
+
+    });
+
+    L.control.liveCoordinates = function(opts) {
+        return new L.Control.LiveMouseCoordinatesControl(opts);
+    };
+
     var googleSheetJsonUrl = 'https://spreadsheets.google.com/feeds/list/1__t--zxgGBV4arofMVay2e9F0K1TzkbkuDLKeZw2iLY/od6/public/values?alt=json'
 
     var attributionHTML = 'Map data &copy; <a href="https://map.playatlas.com/">Grapeshot Games</a>, ';
@@ -26,6 +90,8 @@ $(function(){
     }, {
         "Game-Maps.com Grid": gridMapLayer
     }).addTo(map);
+
+    L.control.liveCoordinates({ position: 'bottomright' }).addTo(map);
 
     $.ajax({
         url: googleSheetJsonUrl
